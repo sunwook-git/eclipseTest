0f01571126e9f2a95339b157a4c0b4ee7391c5cc
sunwook-git <tjsdnr9@gmail.com>
2025-02-28 11:05:50 +0900

    Initial commit

---
+++ src/kosa/mission/practice01.java
@@ -0,0 +1,18 @@
+package kosa.mission;
+
+import java.util.Scanner;
+
+public class practice01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 
+		//임의의 난수 3개 받아서 서로 중첩되지 않게 충력
+		int a,b,c;
+			a = (int)(Math.random() * 15) +1;
+		
+			
+	}
+}
+
